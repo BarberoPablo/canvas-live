6e9051ea9449248947458cdcad5efb563d7a56a0
Pablo Barbero <pablobarbero220@gmail.com>
2025-12-08 15:53:30 -0300

    feat/CL-1: center camera

--- app/components/tldraw/CanvasEditor.tsx
+++ app/components/tldraw/CanvasEditor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Editor, Tldraw, toRichText } from "@tldraw/tldraw";
+import { Editor, Tldraw } from "@tldraw/tldraw";
 import { useRef } from "react";
 import "tldraw/tldraw.css";
 
@@ -8,20 +8,7 @@ export default function CanvasEditor() {
   const editorRef = useRef<Editor | null>(null);
 
   const handleMount = (editor: Editor) => {
-    editor.createShape({
-      type: "text",
-      x: 200,
-      y: 200,
-      props: {
-        richText: toRichText("Hello world!"),
-      },
-    });
-
-    editor.selectAll();
-
-    editor.zoomToSelection({
-      animation: { duration: 5000 },
-    });
+    centerCamera(editor);
   };
 
   return (
@@ -36,3 +23,11 @@ export default function CanvasEditor() {
     </div>
   );
 }
+
+const centerCamera = (editor: Editor) => {
+  editor.selectAll();
+
+  editor.zoomToSelection({
+    animation: { duration: 1000 },
+  });
+};
